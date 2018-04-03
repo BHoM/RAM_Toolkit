024cbeacdf6409e41dd370867ddd04ce320fac2b
Nathan Brown <nathan.brown@burohappold.com>
2018-04-03 17:57:26 -0400

    Implemented converters and read method for basic geometry

--- RAM_Adapter/CRUD/Read.cs
+++ RAM_Adapter/CRUD/Read.cs
@@ -12,6 +12,7 @@ using RAMDATAACCESSLib;
 using System.IO;
 using BH.oM.Geometry;
 
+
 namespace BH.Adapter.RAM
 {
     public partial class RAMAdapter
@@ -30,6 +31,8 @@ namespace BH.Adapter.RAM
                 return ReadSectionProperties(ids as dynamic);
             else if (type == typeof(Material))
                 return ReadMaterials(ids as dynamic);
+            //else if (type == typeof(PanelPlanar))
+            //    return ReadPanels(ids as dynamic);
 
             return null;
         }
@@ -43,15 +46,6 @@ namespace BH.Adapter.RAM
         private List<Bar> ReadBars(List<string> ids = null)
         {
             //Implement code for reading bars
-            
-            
-            // ***Should return list of "Bar", just testing with strings and endpoints
-
-            //Read Columns ---------------------------------
-            
-            List<string> ColumnSections = new List<string>();
-            List<Node> startNodes = new List<Node>();
-            List<Node> endNodes = new List<Node>();
             List<Bar> bhomBars = new List<Bar>();
 
             IModel IModel = m_RAMApplication.GetDispInterfacePointerByEnum(EINTERFACES.IModel_INT);
@@ -60,216 +54,65 @@ namespace BH.Adapter.RAM
             IStories IStories = IModel.GetStories();
             int numStories = IStories.GetCount();
 
-            // Get columns on each story
+            // Get all elements on each story
             for (int i = 0; i < numStories; i++)
             {
+                //Get Columns
                 IColumns IColumns = IStories.GetAt(i).GetColumns();
                 int numColumns = IColumns.GetCount();
 
-                // Find props of every column
-                for (int j = 0; j < IColumns.GetCount(); j++)
-                {
-
-                    // Get the name of every column
-                    IColumn IColumn = IColumns.GetAt(j);
-                    string section = IColumn.strSectionLabel;
-                    ColumnSections.Add(section);
-                    // Get the start and end pts of every column
-                    SCoordinate startPt = new SCoordinate();
-                    SCoordinate endPt = new SCoordinate();
-                    IColumn.GetEndCoordinates(ref startPt, ref endPt);
-                    Node startNode = new Node();
-                    Node endNode = new Node();
-                    startNode.Position = new BH.oM.Geometry.Point() { X = startPt.dXLoc, Y = startPt.dYLoc, Z = startPt.dZLoc };
-                    endNode.Position = new BH.oM.Geometry.Point() { X = endPt.dXLoc, Y = endPt.dYLoc, Z = endPt.dZLoc };
-                    startNodes.Add(startNode);
-                    endNodes.Add(endNode);
-                }
-
-            }
-
-            for (int i = 0; i < ColumnSections.Count(); i++)
-            {
-                //Node startNode = null; 
-                //Node endNode = null; 
-                Bar bhomBar = new Bar { StartNode = startNodes[i], EndNode = endNodes[i], Name = ColumnSections[i] };
-
-                bhomBar.OrientationAngle = 0;
-
-                bhomBars.Add(bhomBar);
-
-            }
-
-
-            //Read Beams ---------------------------------
-
-            List<string> BeamSections = new List<string>();
-            List<Node> startNodesBeams = new List<Node>();
-            List<Node> endNodesBeams = new List<Node>();
-
-            // Get beams on each story
-            for (int i = 0; i < numStories; i++)
-            {
-                // Need both Beams and LayoutBeams, which is where the coordinates are stored; no z-value in Layout Beams, since they can be associated with multiple floors; have to add z manually
+                //Get Beams
                 ILayoutBeams ILayoutBeams = IStories.GetAt(i).GetFloorType().GetLayoutBeams();
                 IBeams IBeams = IStories.GetAt(i).GetBeams();
                 int numBeams = ILayoutBeams.GetCount();
 
-                // Find props of every beam
-                for (int j = 0; j < IBeams.GetCount(); j++)
-                {
-
-                    // Get the name of every beam
-                    IBeam IBeam = IBeams.GetAt(j);
-                    ILayoutBeam ILayoutBeam = ILayoutBeams.GetAt(j);
-                    string section = IBeam.strSectionLabel;
-                    BeamSections.Add(section);
-                    // Get the start and end pts of every beam
-                    double StartSupportX = new double();
-                    double StartSupportY = new double();
-                    double StartSupportZOffset = new double();
-                    double EndSupportX = new double();
-                    double EndSupportY = new double();
-                    double EndSupportZOffset = new double();
-                    double StoryZ = IStories.GetAt(i).dElevation;
-
-
-                    // Get coordinates from ILayout Beam
-                    ILayoutBeam.GetLayoutCoordinates(out StartSupportX, out StartSupportY, out StartSupportZOffset, out EndSupportX, out EndSupportY, out EndSupportZOffset);
-                    Node startNode = new Node();
-                    Node endNode = new Node();
-                    startNode.Position = new BH.oM.Geometry.Point() { X = StartSupportX, Y = StartSupportY, Z = StoryZ + StartSupportZOffset };
-                    endNode.Position = new BH.oM.Geometry.Point() { X = EndSupportX, Y = EndSupportY, Z = StoryZ + EndSupportZOffset };
-                    startNodesBeams.Add(startNode);
-                    endNodesBeams.Add(endNode);
-                }
-
-            }
-
-            for (int i = 0; i < BeamSections.Count(); i++)
-            {
-                //Node startNode = null; 
-                //Node endNode = null; 
-                Bar bhomBar = new Bar { StartNode = startNodesBeams[i], EndNode = endNodesBeams[i], Name = BeamSections[i] };
-
-                bhomBar.OrientationAngle = 0;
-
-                bhomBars.Add(bhomBar);
-
-            }
-
-
-            //Read Vertical Braces ---------------------------------
-
-            List<string> VBraceSections = new List<string>();
-            List<Node> startNodesVBraces = new List<Node>();
-            List<Node> endNodesVBraces = new List<Node>();
- 
-
-            // Get Vertical Braces on each story
-            for (int i = 0; i < numStories; i++)
-            {
+                //Get Vertical Braces
                 IVerticalBraces IVBraces = IStories.GetAt(i).GetVerticalBraces();
                 int numVBraces = IVBraces.GetCount();
 
-                // Find props of every VBrace
-                for (int j = 0; j < numVBraces; j++)
-                {
-
-                    // Get the name of every VBrace
-                    IVerticalBrace IVBrace = IVBraces.GetAt(j);
-                    string section = IVBrace.strSectionLabel;
-                    VBraceSections.Add(section);
-                    // Get the start and end pts of every VBrace
-                    SCoordinate startPt = new SCoordinate();
-                    SCoordinate endPt = new SCoordinate();
-                    IVBrace.GetEndCoordinates(ref startPt, ref endPt);
-                    Node startNode = new Node();
-                    Node endNode = new Node();
-                    startNode.Position = new BH.oM.Geometry.Point() { X = startPt.dXLoc, Y = startPt.dYLoc, Z = startPt.dZLoc };
-                    endNode.Position = new BH.oM.Geometry.Point() { X = endPt.dXLoc, Y = endPt.dYLoc, Z = endPt.dZLoc };
-                    startNodesVBraces.Add(startNode);
-                    endNodesVBraces.Add(endNode);
-                }
-
-            }
-
-            for (int i = 0; i < VBraceSections.Count(); i++)
-            {
-                //Node startNode = null; 
-                //Node endNode = null; 
-                Bar bhomBar = new Bar { StartNode = startNodesVBraces[i], EndNode = endNodesVBraces[i], Name = VBraceSections[i] };
-
-                bhomBar.OrientationAngle = 0;
-
-                bhomBars.Add(bhomBar);
-
-            }
-
-
-
-            //Read Horizontal Braces ---------------------------------
-
-            List<string> HBraceSections = new List<string>();
-            List<Node> startNodesHBraces = new List<Node>();
-            List<Node> endNodesHBraces = new List<Node>();
-
-            // Get HBrace on each story
-            for (int i = 0; i < numStories; i++)
-            {
-                // Like beams, need both ILayout and Regular
+                //Get Horizontal Braces
                 ILayoutHorizBraces ILayoutHorizBraces = IStories.GetAt(i).GetFloorType().GetLayoutHorizBraces();
                 IHorizBraces IHorizBraces = IStories.GetAt(i).GetHorizBraces();
-                int numBraces = ILayoutHorizBraces.GetCount();
+                int numHBraces = ILayoutHorizBraces.GetCount();
 
-                // Find props of every HBrace
-                for (int j = 0; j < IHorizBraces.GetCount(); j++)
+                //Get Elevation
+                double dElevation = IStories.GetAt(i).dElevation;
+
+                // Convert Columns
+                for (int j = 0; j < numColumns; j++)
                 {
+                    IColumn IColumn = IColumns.GetAt(j);
+                    Bar bhomBar = BH.Engine.RAM.Convert.ToBHoMObject(IColumn);
+                    bhomBars.Add(bhomBar);
+                }
 
-                    // Get the name of every HBrace
+                // Convert Beams
+                for (int j = 0; j < numBeams; j++)
+                {
+                    IBeam IBeam = IBeams.GetAt(j);
+                    ILayoutBeam ILayoutBeam = ILayoutBeams.GetAt(j);
+                    Bar bhomBar = BH.Engine.RAM.Convert.ToBHoMObject(IBeam, ILayoutBeam, dElevation);
+                    bhomBars.Add(bhomBar);
+                }
+
+                // Convert Vertical Braces
+                for (int j = 0; j < numVBraces; j++)
+                {
+                    IVerticalBrace IVerticalBrace = IVBraces.GetAt(j);
+                    Bar bhomBar = BH.Engine.RAM.Convert.ToBHoMObject(IVerticalBrace);
+                    bhomBars.Add(bhomBar);
+                }
+
+                // Convert Horizontal Braces
+                for (int j = 0; j < numHBraces; j++)
+                {
                     IHorizBrace IHorizBrace = IHorizBraces.GetAt(j);
                     ILayoutHorizBrace ILayoutHorizBrace = ILayoutHorizBraces.GetAt(j);
-                    string section = IHorizBrace.strSectionLabel;
-                    HBraceSections.Add(section);
-                    // Get the start and end pts of every HBrace
-                    double StartSupportX = new double();
-                    double StartSupportY = new double();
-                    double StartSupportZOffset = new double();
-                    double EndSupportX = new double();
-                    double EndSupportY = new double();
-                    double EndSupportZOffset = new double();
-                    double StoryZ = IStories.GetAt(i).dElevation;
-
-
-                    // Get coordinates from ILayoutHorizBrace
-                    ILayoutHorizBrace.GetLayoutCoordinates(out StartSupportX, out StartSupportY, out StartSupportZOffset, out EndSupportX, out EndSupportY, out EndSupportZOffset);
-                    Node startNode = new Node();
-                    Node endNode = new Node();
-                    startNode.Position = new BH.oM.Geometry.Point() { X = StartSupportX, Y = StartSupportY, Z = StoryZ + StartSupportZOffset };
-                    endNode.Position = new BH.oM.Geometry.Point() { X = EndSupportX, Y = EndSupportY, Z = StoryZ + EndSupportZOffset };
-                    startNodesHBraces.Add(startNode);
-                    endNodesHBraces.Add(endNode);
+                    Bar bhomBar = BH.Engine.RAM.Convert.ToBHoMObject(IHorizBrace, ILayoutHorizBrace, dElevation);
+                    bhomBars.Add(bhomBar);
                 }
 
             }
-
-            for (int i = 0; i < HBraceSections.Count(); i++)
-            {
-                //Node startNode = null; 
-                //Node endNode = null; 
-                Bar bhomBar = new Bar { StartNode = startNodesHBraces[i], EndNode = endNodesHBraces[i], Name = HBraceSections[i] };
-
-                bhomBar.OrientationAngle = 0;
-
-                bhomBars.Add(bhomBar);
-
-            }
-
-
-
-
-
-
 
             return bhomBars;
 
@@ -300,10 +143,44 @@ namespace BH.Adapter.RAM
         {
             //Implement code for reading materials
             throw new NotImplementedException();
+
         }
 
         /***************************************************/
 
+        //// Read panels method; will need to figure out how to convert geometry (RAM provides four corner points); does not seem to be working properly, crashes Rhino when walls are present
+        //private List<PanelPlanar> ReadPanels(List<string> ids = null)
+        //{
+        //    //Implement code for reading panels
+        //    List<PanelPlanar> bhomPanels = new List<PanelPlanar>();
 
+        //    IModel IModel = m_RAMApplication.GetDispInterfacePointerByEnum(EINTERFACES.IModel_INT);
+
+        //    // Get stories
+        //    IStories IStories = IModel.GetStories();
+        //    int numStories = IStories.GetCount();
+
+        //    // Get all elements on each story
+        //    for (int i = 0; i < numStories; i++)
+        //    {
+
+        //        //Get walls
+        //        IWalls IWalls = IStories.GetAt(i).GetWalls();
+        //        int numWalls = IWalls.GetCount();
+
+        //        return bhomPanels;
+
+        //        // Convert Walls
+        //        for (int j = 0; j < numWalls; j++)
+        //        {
+        //            IWall IWall = IWalls.GetAt(j);
+        //            PanelPlanar Panel = BH.Engine.RAM.Convert.ToBHoMObject(IWall);
+        //            bhomPanels.Add(Panel);
+        //        }
+
+        //    }
+
+        //    return bhomPanels;
+        //}
     }
 }
--- RAM_Engine/Convert/ToBHoM.cs
+++ RAM_Engine/Convert/ToBHoM.cs
@@ -24,5 +24,170 @@ namespace BH.Engine.RAM
 
         /***************************************************/
 
+        public static Bar ToBHoMObject(IColumn IColumn)
+        {
+
+            // Get the column name
+            string section = IColumn.strSectionLabel;
+            
+            // Get the start and end pts of every column
+            SCoordinate startPt = new SCoordinate();
+            SCoordinate endPt = new SCoordinate();
+            IColumn.GetEndCoordinates(ref startPt, ref endPt);
+            Node startNode = new Node();
+            Node endNode = new Node();
+            startNode.Position = new BH.oM.Geometry.Point() { X = startPt.dXLoc, Y = startPt.dYLoc, Z = startPt.dZLoc };
+            endNode.Position = new BH.oM.Geometry.Point() { X = endPt.dXLoc, Y = endPt.dYLoc, Z = endPt.dZLoc };
+            
+
+            Bar bhomBar = new Bar { StartNode = startNode, EndNode = endNode, Name = section };
+
+            bhomBar.OrientationAngle = 0;
+
+            return  bhomBar;
+        }
+
+        public static Bar ToBHoMObject(IBeam IBeam, ILayoutBeam ILayoutBeam, double dElevation)
+        {
+
+            string section = IBeam.strSectionLabel;
+
+            //// Get Steel beam results **STILL IN PROGRESS
+            //ISteelBeamDesignResult Result = IBeam.GetSteelDesignResult();
+            //DAArray ppalNumStuds = Result.GetNumStudsInSegments();
+            //int numStudSegments = new int();
+            //ppalNumStuds.GetSize(ref numStudSegments);
+            //double Camber = IBeam.dCamber;
+
+            // Get the start and end pts of every beam
+            double StartSupportX = new double();
+            double StartSupportY = new double();
+            double StartSupportZOffset = new double();
+            double EndSupportX = new double();
+            double EndSupportY = new double();
+            double EndSupportZOffset = new double();
+            double StoryZ = dElevation;
+         
+
+            // Get coordinates from ILayout Beam
+            ILayoutBeam.GetLayoutCoordinates(out StartSupportX, out StartSupportY, out StartSupportZOffset, out EndSupportX, out EndSupportY, out EndSupportZOffset);
+            Node startNode = new Node();
+            Node endNode = new Node();
+            startNode.Position = new BH.oM.Geometry.Point() { X = StartSupportX, Y = StartSupportY, Z = StoryZ + StartSupportZOffset };
+            endNode.Position = new BH.oM.Geometry.Point() { X = EndSupportX, Y = EndSupportY, Z = StoryZ + EndSupportZOffset };
+
+            Bar bhomBar = new Bar { StartNode = startNode, EndNode = endNode, Name = section };
+
+            bhomBar.OrientationAngle = 0;
+
+            //// Add studs to custom Data  *STILL IN PROGRESS, gives "specific cast is not valid" error through Grasshopper when turned on
+            //for (int i = 0; i < numStudSegments; i++)
+            //{
+            //    var pvrtItem = new object();
+            //    string numStuds = ppalNumStuds.GetAt(i, pvrtItem).ToString();
+
+            //    //Check to make sure studs are set
+            //    if ( (bool) pvrtItem)
+            //    {
+            //        bhomBar.CustomData["Studs" + i.ToString()] = numStuds;
+            //    }
+            //}
+
+            //// Add camber to Custom Data
+            //if (Camber > Double.MinValue)
+            //{
+            //    bhomBar.CustomData["Camber"] = Camber;
+            //    //bhomBar.CustomData.Add("Camber", Camber);
+            //}
+
+            //bhomBar.CustomData["Test1"] = "Object1";
+            //bhomBar.CustomData.Add("Test1", "Object1_2");
+            //bhomBar.CustomData["Test2"] = "Object2";
+            //bhomBar.CustomData.Add("Test2", "Object2_2");
+
+            return bhomBar;
+        }
+
+        public static Bar ToBHoMObject(IVerticalBrace IVerticalBrace)
+        {
+
+            // Get the column name
+            string section = IVerticalBrace.strSectionLabel;
+
+            // Get the start and end pts of every column
+            SCoordinate startPt = new SCoordinate();
+            SCoordinate endPt = new SCoordinate();
+            IVerticalBrace.GetEndCoordinates(ref startPt, ref endPt);
+            Node startNode = new Node();
+            Node endNode = new Node();
+            startNode.Position = new BH.oM.Geometry.Point() { X = startPt.dXLoc, Y = startPt.dYLoc, Z = startPt.dZLoc };
+            endNode.Position = new BH.oM.Geometry.Point() { X = endPt.dXLoc, Y = endPt.dYLoc, Z = endPt.dZLoc };
+
+
+            Bar bhomBar = new Bar { StartNode = startNode, EndNode = endNode, Name = section };
+
+            bhomBar.OrientationAngle = 0;
+
+            return bhomBar;
+        }
+
+        public static Bar ToBHoMObject(IHorizBrace IHorizBrace, ILayoutHorizBrace ILayoutHorizBrace, double dElevation)
+        {
+
+            string section = IHorizBrace.strSectionLabel;
+
+            // Get the start and end pts of every beam
+            double StartSupportX = new double();
+            double StartSupportY = new double();
+            double StartSupportZOffset = new double();
+            double EndSupportX = new double();
+            double EndSupportY = new double();
+            double EndSupportZOffset = new double();
+            double StoryZ = dElevation;
+
+
+            // Get coordinates from ILayout Beam
+            ILayoutHorizBrace.GetLayoutCoordinates(out StartSupportX, out StartSupportY, out StartSupportZOffset, out EndSupportX, out EndSupportY, out EndSupportZOffset);
+            Node startNode = new Node();
+            Node endNode = new Node();
+            startNode.Position = new BH.oM.Geometry.Point() { X = StartSupportX, Y = StartSupportY, Z = StoryZ + StartSupportZOffset };
+            endNode.Position = new BH.oM.Geometry.Point() { X = EndSupportX, Y = EndSupportY, Z = StoryZ + EndSupportZOffset };
+
+            Bar bhomBar = new Bar { StartNode = startNode, EndNode = endNode, Name = section };
+
+            bhomBar.OrientationAngle = 0;
+
+            return bhomBar;
+        }
+
+
+        public static PanelPlanar ToBHoMObject(IWall IWall)
+        {
+
+            List<string> CustomProps = new List<string>();
+            double thickness = IWall.dThickness;
+            EFRAMETYPE type = IWall.eFramingType;
+            EMATERIALTYPES material = IWall.eMaterial;
+
+            // Get coordinates from ILayout Beam
+            PanelPlanar bhomPanel = new PanelPlanar();
+
+            SCoordinate TopstartPt = new SCoordinate();
+            SCoordinate TopendPt = new SCoordinate();
+            SCoordinate BottomstartPt = new SCoordinate();
+            SCoordinate BottomendPt = new SCoordinate();
+
+            IWall.GetEndCoordinates(ref TopstartPt, ref TopendPt, ref BottomstartPt, ref BottomendPt);
+
+            HashSet<String> tag = new HashSet<string>();
+            tag.Add("Wall");
+
+            bhomPanel.Tags = tag;
+            bhomPanel.Name = thickness.ToString() + " " + material.ToString();
+
+            return bhomPanel;
+        }
+
+
     }
 }
