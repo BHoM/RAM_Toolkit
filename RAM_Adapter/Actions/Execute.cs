8bf077c3a4adef05f8c1251aa64e3ddf00a5be74
Josef Taylor <josef.taylor@burohappold.com>
2019-11-06 13:45:47 -0500

    Added closer

---
+++ RAM_Adapter/Actions/Execute.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BH.Adapter.RAM
+{
+    public abstract partial class RAMAdapter
+    {
+        public override bool Execute(string command, Dictionary<string, object> parameters = null, Dictionary<string, object> config = null)
+        {
+            command = command.ToUpper();
+
+            switch (command)
+            {
+                case ("CLOSE"):
+                    {
+                        if (RAMDataAccIDBIO != null)
+                        {
+                            RAMDataAccIDBIO.SaveDatabase();
+                            RAMDataAccIDBIO.CloseDatabase();
+                        }
+
+                        RAMDataAccIDBIO = null;
+                        m_RAMApplication = null;
+
+                        return true;
+                    }
+                default:
+                    {
+                        return false;
+                    }
+            }
+        }
+    }
+}
